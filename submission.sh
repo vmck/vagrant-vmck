a62922e8147db6c660053176e8be896ad096a86e
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-06 11:18:13 +0300

    Encode output with base64

--- submission.sh
+++ submission.sh
@@ -2,25 +2,9 @@
 
 trap "vagrant destroy -f" EXIT
 
-# takes a file as input and returns a JSON list
-function make_list()
-{
-    _output="["
-
-    while IFS= read -r line
-    do
-        line=$(echo ${line} | tr -cd '[:print:]')
-        _output=${_output}"\"${line}\","
-    done < $1
-
-    _output=${_output}"\"\"]"
-
-    echo ${_output}
-}
-
 curl -X GET ${DOWNLOAD_URL} > archive.zip
 unzip archive.zip
 vagrant up
 vagrant ssh -- < checker.sh > result.out
-data=$(make_list ./result.out)
-curl -X POST 'http://10.42.1.1:10002/done/' -d "{\"token\": \"${DOWNLOAD_URL}\", \"output\": ${data}}"
+data="$(base64 result.out)"
+curl -X POST 'http://10.42.1.1:10002/done/' -d "{\"token\": \"${DOWNLOAD_URL}\", \"output\": \"${data}\"}"
