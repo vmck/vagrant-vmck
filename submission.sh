b4ea41599f3fcbfdf8e3684c87312099f551be66
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-06 23:13:34 +0300

    Fix sent data

--- submission.sh
+++ submission.sh
@@ -7,4 +7,4 @@ curl -X GET ${DOWNLOAD_SCRIPT_URL} > checker.sh
 vagrant up
 vagrant ssh -- < checker.sh > result.out
 data="$(base64 result.out)"
-curl -X POST 'http://10.42.1.1:10002/done/' -d "{\"token\": \"${DOWNLOAD_URL}\", \"output\": \"${data}\"}"
+curl -X POST 'http://10.42.1.1:10002/done/' -d "{\"token\": \"${DOWNLOAD_ARCHIVE_URL}\", \"output\": \"${data}\"}"
