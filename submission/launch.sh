13fac8b456617aa7d8f07e8235d60fe9f8105c22
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-13 12:21:28 +0300

    Cd into current dir

--- submission/launch.sh
+++ submission/launch.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -ex
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"/..
+
 trap "vagrant destroy -f" EXIT
+
 curl "${ARCHIVE_URL}" -o submission.zip
 curl "${SCRIPT_URL}" -o checker.sh
+
 vagrant up
 vagrant ssh -- < checker.sh > result.out
+
 data="$(base64 result.out)"
 JSON_STRING=$(jq -n \
                  --arg tok "$SUBMISSION_ID" \
