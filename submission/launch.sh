acc34465e3e481ba739eff00b632d17005930d8f
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-17 14:53:05 +0300

    Changes

--- submission/launch.sh
+++ submission/launch.sh
@@ -1,18 +1,23 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 trap "vagrant destroy -f" EXIT
 
-curl "${VMCK_ARCHIVE_URL}" -o submission.zip
-curl "${VMCK_SCRIPT_URL}" -o checker.sh
+curl "${VMCK_ARCHIVE_URL}" -o archive.zip
+curl "${VMCK_SCRIPT_URL}" -o script.sh
 
 vagrant up
-vagrant ssh -- < checker.sh > result.out
+vagrant ssh -- < script.sh 1> result.out 2> result.err
 
-data="$(base64 result.out)"
+exit_code=$?
+stdout="$(base64 result.out)"
+stderr="$(base64 reuslt.err)"
+
 JSON_STRING=$(jq -n \
-                 --arg out "$data" \
-                 '{output: $out,}')
+                 --arg out "$stdout" \
+                 --arg err "$stderr" \
+                 --arg code $exit_code \
+                 '{stdout: $out, stderr: $err, exit_code: $code},')
 curl -X POST "${VMCK_CALLBACK_URL}" -d "$JSON_STRING" \
      --header "Content-Type: application/json"
